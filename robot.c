0c230037d83e5e497a007b0e6db01d8169de26a8
Justin Ohta <ohta17@up.edu>
2014-10-22 23:22:05 +0000

    Gave the robot a facelift. Added myself to authorlist.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw
+ * @author Justin Ohta
  * @since August 2013
  *
  */
@@ -45,7 +45,7 @@ void robPrintMessage(int num)
 void robPrintAscii(void)
 {
   printf("    i_i    \n"
-	 "   [@_@]   \n"
+	 "   [T_T]   \n"
 	 "  /|___|\\  \n"
          "   d   b   "); 
 
